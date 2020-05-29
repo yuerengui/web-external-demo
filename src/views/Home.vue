c00e89c0de1444f1cda86d78a7df6f7680f75bd9
yuerengui <596580041@qq.com>
2020-05-29 15:55:54 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  created() {
+    this.$axios.get('https://hkdev.external.housesigma.com/apiex/index').then((response) => {
+      console.log(response)
+    })
+  }
+}
+</script>
