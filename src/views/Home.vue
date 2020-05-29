0feda4a1af1051307918a042f23e6289d76867c1
yuerengui <596580041@qq.com>
2020-05-29 17:54:24 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <span v-if="user && user.email">{{ user.email }}</span>
     <a href="https://hkdev.external.housesigma.com/apiex/auth/login" target="_blank" v-else>Login</a>
     <button @click="logout" v-if="user && user.email">logout</button>
+    <br/><br/>
+    <button @click="profile">get profile</button>
     <textarea name="" id="" cols="30" rows="10" v-model="test_ajax">
     </textarea>
   </div>
